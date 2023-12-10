3414133ca43aaa63ed2f0e942b3551ca7e47ffd1
AlexandreMisrahi2005 <alex.misrahi@icloud.com>
2023-12-10 11:57:40 +0100

    Update backbone_embedding_size_5_shot.sh

--- scripts/backbone_embedding_size_5_shot.sh
+++ scripts/backbone_embedding_size_5_shot.sh
@@ -16,6 +16,9 @@ python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptne
 python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_1_maxiter_3_embedding_64_32 method.base_learner=svm method.C_reg=1 backbone.layer_dim=[64,32]
 
 echo "5-shot metaoptnet-svm with 40 epochs, backbone sizes = [64, 128], varying C_reg"
+python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_100_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=100 backbone.layer_dim=[64,128]
+python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_50_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=50 backbone.layer_dim=[64,128]
+python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_20_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=20 backbone.layer_dim=[64,128]
 python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_10_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=10 backbone.layer_dim=[64,128]
 python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_5_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=5 backbone.layer_dim=[64,128]
 python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_1_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=1 backbone.layer_dim=[64,128]
@@ -29,4 +32,4 @@ echo "5-shot metaoptnet-ridge with 40 epochs, backbone sizes = [64, 128], varyin
 python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_10_embedding_64_128 method.base_learner=ridge method.lambda_reg=10 backbone.layer_dim=[64,128]
 python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_5_embedding_64_128 method.base_learner=ridge method.lambda_reg=5 backbone.layer_dim=[64,128]
 python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_1_embedding_64_128 method.base_learner=ridge method.lambda_reg=1 backbone.layer_dim=[64,128]
-
+python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_01_embedding_64_128 method.base_learner=ridge method.lambda_reg=0.1 backbone.layer_dim=[64,128]
