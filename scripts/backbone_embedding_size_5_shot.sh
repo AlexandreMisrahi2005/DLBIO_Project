f2a1e1335dcdbc2bf1d733c4ad683e9a7cd3e3d3
AlexandreMisrahi2005 <alex.misrahi@icloud.com>
2023-12-11 11:20:09 +0100

    add swiss prot scripts
    add swiss prot scripts for 5-shot with varying embedding sizes
    (+ correct name in other script)

--- scripts/backbone_embedding_size_5_shot.sh
+++ scripts/backbone_embedding_size_5_shot.sh
@@ -1,35 +1,72 @@
 #!/bin/bash
 
-echo "5-shot methods with 40 epochs, backbone sizes = [64, 32]"
-python run.py dataset=tabula_muris method=matchingnet n_shot=5 exp.name=matchingnet_tabula_muris_5_shot_embedding_64_32 backbone.layer_dim=[64,32]
-python run.py dataset=tabula_muris method=protonet n_shot=5 exp.name=protonet_tabula_muris_5_shot_embedding_64_32 backbone.layer_dim=[64,32]
-python run.py dataset=tabula_muris method=maml n_shot=5 exp.name=maml_tabula_muris_5_shot_5_updates_innerlr_002_embedding_64_32 method.task_update_num=5 method.maml_inner_lr=0.002 backbone.layer_dim=[64,32]
+# echo "TABULA MURIS"
+# echo "5-shot methods with 40 epochs, backbone sizes = [64, 32]"
+# python run.py dataset=tabula_muris method=matchingnet n_shot=5 exp.name=matchingnet_tabula_muris_5_shot_embedding_64_32 backbone.layer_dim=[64,32]
+# python run.py dataset=tabula_muris method=protonet n_shot=5 exp.name=protonet_tabula_muris_5_shot_embedding_64_32 backbone.layer_dim=[64,32]
+# python run.py dataset=tabula_muris method=maml n_shot=5 exp.name=maml_tabula_muris_5_shot_5_updates_innerlr_002_embedding_64_32 method.task_update_num=5 method.maml_inner_lr=0.002 backbone.layer_dim=[64,32]
 
-echo "5-shot methods with 40 epochs, backbone sizes = [64, 128]"
-python run.py dataset=tabula_muris method=matchingnet n_shot=5 exp.name=matchingnet_tabula_muris_5_shot_embedding_64_128 backbone.layer_dim=[64,128]
-python run.py dataset=tabula_muris method=protonet n_shot=5 exp.name=protonet_tabula_muris_5_shot_embedding_64_128 backbone.layer_dim=[64,128]
-python run.py dataset=tabula_muris method=maml n_shot=5 exp.name=maml_tabula_muris_5_shot_5_updates_innerlr_002_embedding_64_128 method.task_update_num=5 method.maml_inner_lr=0.002 backbone.layer_dim=[64,128]
+# echo "5-shot methods with 40 epochs, backbone sizes = [64, 128]"
+# python run.py dataset=tabula_muris method=matchingnet n_shot=5 exp.name=matchingnet_tabula_muris_5_shot_embedding_64_128 backbone.layer_dim=[64,128]
+# python run.py dataset=tabula_muris method=protonet n_shot=5 exp.name=protonet_tabula_muris_5_shot_embedding_64_128 backbone.layer_dim=[64,128]
+# python run.py dataset=tabula_muris method=maml n_shot=5 exp.name=maml_tabula_muris_5_shot_5_updates_innerlr_002_embedding_64_128 method.task_update_num=5 method.maml_inner_lr=0.002 backbone.layer_dim=[64,128]
 
-echo "5-shot metaoptnet-svm with 40 epochs, backbone sizes = [64, 32], varying C_reg"
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_10_maxiter_3_embedding_64_32 method.base_learner=svm method.C_reg=10 backbone.layer_dim=[64,32]
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_5_maxiter_3_embedding_64_32 method.base_learner=svm method.C_reg=5 backbone.layer_dim=[64,32]
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_1_maxiter_3_embedding_64_32 method.base_learner=svm method.C_reg=1 backbone.layer_dim=[64,32]
+# echo "5-shot metaoptnet-svm with 40 epochs, backbone sizes = [64, 32], varying C_reg"
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_10_maxiter_3_embedding_64_32 method.base_learner=svm method.C_reg=10 backbone.layer_dim=[64,32]
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_5_maxiter_3_embedding_64_32 method.base_learner=svm method.C_reg=5 backbone.layer_dim=[64,32]
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_1_maxiter_3_embedding_64_32 method.base_learner=svm method.C_reg=1 backbone.layer_dim=[64,32]
 
-echo "5-shot metaoptnet-svm with 40 epochs, backbone sizes = [64, 128], varying C_reg"
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_100_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=100 backbone.layer_dim=[64,128]
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_50_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=50 backbone.layer_dim=[64,128]
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_20_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=20 backbone.layer_dim=[64,128]
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_10_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=10 backbone.layer_dim=[64,128]
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_5_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=5 backbone.layer_dim=[64,128]
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_1_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=1 backbone.layer_dim=[64,128]
+# echo "5-shot metaoptnet-svm with 40 epochs, backbone sizes = [64, 128], varying C_reg"
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_100_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=100 backbone.layer_dim=[64,128]
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_50_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=50 backbone.layer_dim=[64,128]
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_20_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=20 backbone.layer_dim=[64,128]
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_10_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=10 backbone.layer_dim=[64,128]
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_5_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=5 backbone.layer_dim=[64,128]
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_tabula_muris_5_shot_C_reg_1_maxiter_3_embedding_64_128 method.base_learner=svm method.C_reg=1 backbone.layer_dim=[64,128]
 
-echo "5-shot metaoptnet-ridge with 40 epochs, backbone sizes = [64, 32], varying lambda_reg"
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_10_embedding_64_32 method.base_learner=ridge method.lambda_reg=10 backbone.layer_dim=[64,32]
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_5_embedding_64_32 method.base_learner=ridge method.lambda_reg=5 backbone.layer_dim=[64,32]
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_1_embedding_64_32 method.base_learner=ridge method.lambda_reg=1 backbone.layer_dim=[64,32]
+# echo "5-shot metaoptnet-ridge with 40 epochs, backbone sizes = [64, 32], varying lambda_reg"
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_10_embedding_64_32 method.base_learner=ridge method.lambda_reg=10 backbone.layer_dim=[64,32]
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_5_embedding_64_32 method.base_learner=ridge method.lambda_reg=5 backbone.layer_dim=[64,32]
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_1_embedding_64_32 method.base_learner=ridge method.lambda_reg=1 backbone.layer_dim=[64,32]
 
-echo "5-shot metaoptnet-ridge with 40 epochs, backbone sizes = [64, 128], varying lambda_reg"
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_10_embedding_64_128 method.base_learner=ridge method.lambda_reg=10 backbone.layer_dim=[64,128]
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_5_embedding_64_128 method.base_learner=ridge method.lambda_reg=5 backbone.layer_dim=[64,128]
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_1_embedding_64_128 method.base_learner=ridge method.lambda_reg=1 backbone.layer_dim=[64,128]
-python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_01_embedding_64_128 method.base_learner=ridge method.lambda_reg=0.1 backbone.layer_dim=[64,128]
+# echo "5-shot metaoptnet-ridge with 40 epochs, backbone sizes = [64, 128], varying lambda_reg"
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_10_embedding_64_128 method.base_learner=ridge method.lambda_reg=10 backbone.layer_dim=[64,128]
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_5_embedding_64_128 method.base_learner=ridge method.lambda_reg=5 backbone.layer_dim=[64,128]
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_1_embedding_64_128 method.base_learner=ridge method.lambda_reg=1 backbone.layer_dim=[64,128]
+# python run.py dataset=tabula_muris method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_tabula_muris_5_shot_lambda_reg_01_embedding_64_128 method.base_learner=ridge method.lambda_reg=0.1 backbone.layer_dim=[64,128]
+
+
+echo "SWISS PROT"
+echo "5-shot methods with 40 epochs, backbone sizes = [512, 256]"
+python run.py dataset=swissprot method=matchingnet n_shot=5 exp.name=matchingnet_swissprot_5_shot_embedding_512_256 backbone.layer_dim=[512,256]
+python run.py dataset=swissprot method=protonet n_shot=5 exp.name=protonet_swissprot_5_shot_embedding_512_256 backbone.layer_dim=[512,256]
+python run.py dataset=swissprot method=maml n_shot=5 exp.name=maml_swissprot_5_shot_5_updates_innerlr_002_embedding_512_256 method.task_update_num=5 method.maml_inner_lr=0.002 backbone.layer_dim=[512,256]
+
+echo "5-shot methods with 40 epochs, backbone sizes = [512, 1024]"
+python run.py dataset=swissprot method=matchingnet n_shot=5 exp.name=matchingnet_swissprot_5_shot_embedding_512_1024 backbone.layer_dim=[512,1024]
+python run.py dataset=swissprot method=protonet n_shot=5 exp.name=protonet_swissprot_5_shot_embedding_512_1024 backbone.layer_dim=[512,1024]
+python run.py dataset=swissprot method=maml n_shot=5 exp.name=maml_swissprot_5_shot_5_updates_innerlr_002_embedding_512_1024 method.task_update_num=5 method.maml_inner_lr=0.002 backbone.layer_dim=[512,1024]
+
+echo "5-shot metaoptnet-svm with 40 epochs, backbone sizes = [512, 256], varying C_reg"
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_swissprot_5_shot_C_reg_10_maxiter_3_embedding_512_256 method.base_learner=svm method.C_reg=10 backbone.layer_dim=[512,256]
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_swissprot_5_shot_C_reg_5_maxiter_3_embedding_512_256 method.base_learner=svm method.C_reg=5 backbone.layer_dim=[512,256]
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_swissprot_5_shot_C_reg_1_maxiter_3_embedding_512_256 method.base_learner=svm method.C_reg=1 backbone.layer_dim=[512,256]
+
+echo "5-shot metaoptnet-svm with 40 epochs, backbone sizes = [512, 1024], varying C_reg"
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_swissprot_5_shot_C_reg_100_maxiter_3_embedding_512_1024 method.base_learner=svm method.C_reg=100 backbone.layer_dim=[512,1024]
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_swissprot_5_shot_C_reg_50_maxiter_3_embedding_512_1024 method.base_learner=svm method.C_reg=50 backbone.layer_dim=[512,1024]
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_swissprot_5_shot_C_reg_20_maxiter_3_embedding_512_1024 method.base_learner=svm method.C_reg=20 backbone.layer_dim=[512,1024]
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_swissprot_5_shot_C_reg_10_maxiter_3_embedding_512_1024 method.base_learner=svm method.C_reg=10 backbone.layer_dim=[512,1024]
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_swissprot_5_shot_C_reg_5_maxiter_3_embedding_512_1024 method.base_learner=svm method.C_reg=5 backbone.layer_dim=[512,1024]
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_svm_swissprot_5_shot_C_reg_1_maxiter_3_embedding_512_1024 method.base_learner=svm method.C_reg=1 backbone.layer_dim=[512,1024]
+
+echo "5-shot metaoptnet-ridge with 40 epochs, backbone sizes = [512, 256], varying lambda_reg"
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_swissprot_5_shot_lambda_reg_10_embedding_512_256 method.base_learner=ridge method.lambda_reg=10 backbone.layer_dim=[512,256]
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_swissprot_5_shot_lambda_reg_5_embedding_512_256 method.base_learner=ridge method.lambda_reg=5 backbone.layer_dim=[512,256]
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_swissprot_5_shot_lambda_reg_1_embedding_512_256 method.base_learner=ridge method.lambda_reg=1 backbone.layer_dim=[512,256]
+
+echo "5-shot metaoptnet-ridge with 40 epochs, backbone sizes = [512, 1024], varying lambda_reg"
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_swissprot_5_shot_lambda_reg_10_embedding_512_1024 method.base_learner=ridge method.lambda_reg=10 backbone.layer_dim=[512,1024]
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_swissprot_5_shot_lambda_reg_5_embedding_512_1024 method.base_learner=ridge method.lambda_reg=5 backbone.layer_dim=[512,1024]
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_swissprot_5_shot_lambda_reg_1_embedding_512_1024 method.base_learner=ridge method.lambda_reg=1 backbone.layer_dim=[512,1024]
+python run.py dataset=swissprot method=metaoptnet n_shot=5 exp.name=metaoptnet_ridge_swissprot_5_shot_lambda_reg_01_embedding_512_1024 method.base_learner=ridge method.lambda_reg=0.1 backbone.layer_dim=[512,1024]
--- scripts/tabula-muris-baselines.sh
+++ scripts/tabula-muris-baselines.sh
@@ -15,5 +15,5 @@ python run.py dataset=tabula_muris method=maml n_shot=5 exp.name=maml_tabula_mur
 python run.py dataset=tabula_muris method=matchingnet n_shot=1 exp.name=matchingnet_tabula_muris_1_shot_60_epochs method.stop_epoch=60
 python run.py dataset=tabula_muris method=protonet n_shot=1 exp.name=protonet_tabula_muris_1_shot_60_epochs method.stop_epoch=60
 # MAML-specific parameters: inner LR and number of updates
-python run.py dataset=tabula_muris method=maml n_shot=1 exp.name=maml_tabula_muris_5_shot_1_update_60_epochs method.task_update_num=1 method.maml_inner_lr=0.01 method.stop_epoch=60
-python run.py dataset=tabula_muris method=maml n_shot=1 exp.name=maml_tabula_muris_5_shot_5_updates_innerlr_002_60_epochs method.task_update_num=5 method.maml_inner_lr=0.002 method.stop_epoch=60
+python run.py dataset=tabula_muris method=maml n_shot=1 exp.name=maml_tabula_muris_1_shot_1_update_60_epochs method.task_update_num=1 method.maml_inner_lr=0.01 method.stop_epoch=60
+python run.py dataset=tabula_muris method=maml n_shot=1 exp.name=maml_tabula_muris_1_shot_5_updates_innerlr_002_60_epochs method.task_update_num=5 method.maml_inner_lr=0.002 method.stop_epoch=60
