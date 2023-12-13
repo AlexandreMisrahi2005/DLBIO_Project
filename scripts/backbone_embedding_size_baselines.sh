b28367a25d0da57bf401ec410c048417b2745380
TimBaechle <tim.baechle@tum.de>
2023-12-13 01:10:44 +0100

    Add final runs

--- scripts/backbone_embedding_size_baselines.sh
+++ scripts/backbone_embedding_size_baselines.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+echo "Swiss Prot"
+python run.py method=metaoptnet method.base_learner=svm method.C_reg=20 method.maxIter=1 dataset=swissprot n_shot=5 method.stop_epoch=40 exp.name=svm_C_20_iter_1_swissprot_5_shot
+python run.py method=metaoptnet method.base_learner=svm method.C_reg=50 method.maxIter=1 dataset=swissprot n_shot=5 method.stop_epoch=40 exp.name=svm_C_50_iter_1_swissprot_5_shot
+
+python run.py method=metaoptnet method.base_learner=svm method.C_reg=20 method.maxIter=1 dataset=swissprot n_shot=1 method.stop_epoch=60 exp.name=svm_C_20_iter_1_swissprot_1_shot
+python run.py method=metaoptnet method.base_learner=svm method.C_reg=50 method.maxIter=1 dataset=swissprot n_shot=1 method.stop_epoch=60 exp.name=svm_C_50_iter_1_swissprot_1_shot
+
+python run.py method=metaoptnet method.base_learner=svm method.C_reg=0.5 method.maxIter=10 dataset=swissprot n_shot=1 method.stop_epoch=60 exp.name=svm_C_05_iter_10_swissprot_1_shot
+python run.py method=metaoptnet method.base_learner=svm method.C_reg=0.1 method.maxIter=10 dataset=swissprot n_shot=1 method.stop_epoch=60 exp.name=svm_C_01_iter_10_swissprot_1_shot
+
 echo "TABULA MURIS"
 echo "backbone sizes = [64, 32]"
 echo "5-shot with 40 epochs"
